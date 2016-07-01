0e4f5c25adf5526b49ec283688b1cb405521a70e
Jeff Portwood <jportwood@findthebest.com>
2016-07-01 14:53:13 -0700

    Tests: update tests

--- tests/KettleParserTests.py
+++ tests/KettleParserTests.py
@@ -7,26 +7,42 @@ __author__ = 'jeffportwood'
 
 class KettleParserTests(unittest.TestCase):
 
+
     def test_parse_xml_file(self):
-        trans = KettleParser.ParseKettleXml(constants.TRANSFORMATION_1_FILE)
+        trans = KettleParser.Parse(constants.TRANSFORMATION_1_FILE)
 
         # Steps
-        self.assertDictEqual(trans.steps, constants.TRANSFORMATION_1_STEPS, "Error parsing steps")
+        step_dict = {}
+        for step in trans.steps:
+            step_dict[step.get_attribute("name")] = {"type": step.get_attribute("type")}
+        self.assertDictEqual(step_dict, constants.TRANSFORMATION_1_STEPS, "Error parsing steps")
 
         # Step attributes
         test_steps = copy.deepcopy(constants.TRANSFORMATION_1_STEPS)
         test_steps["Text file input"]["format"] = "Unix"
-        trans.get_step_attribute("Text file input", "format")
-        self.assertDictEqual(trans.steps, test_steps, "Error parsing steps")
+        for step in trans.steps:
+            if step.get_attribute("name") == "Text file input":
+                step_dict["Text file input"]["format"] = step.get_attribute("format")
+        self.assertDictEqual(step_dict, test_steps, "Error parsing steps")
 
         # Hops
-        self.assertListEqual(trans.hops, constants.TRANSFORMATION_1_HOPS, "Error parsing hops")
+        hop_list = []
+        for hop in trans.hops:
+            hop_list.append({"to": hop.get_attribute("to"),
+                             'from': hop.get_attribute("from"),
+                             'enabled': hop.get_attribute("enabled")})
+        self.assertListEqual(hop_list, constants.TRANSFORMATION_1_HOPS, "Error parsing hops")
+
+
+    def test_kettle_graph(self):
+        trans = KettleParser.Parse(constants.TRANSFORMATION_1_FILE)
+        kettle_graph = KettleParser.KettleGraph(trans.hops)
 
         # Graph
-        self.assertDictEqual(trans.graph, constants.TRANSFORMATION_1_GRAPH, "Error parsing graph")
+        self.assertDictEqual(kettle_graph.graph, constants.TRANSFORMATION_1_GRAPH, "Error parsing graph")
 
         # Paths
-        paths = [path for path in trans.find_all_paths("Text file input", "Text file output")]
+        paths = [path for path in kettle_graph.find_all_paths("Text file input", "Text file output")]
         self.assertListEqual(paths, constants.TRANSFORMATION_1_PATHS)
 
 
--- tests/constants.py
+++ tests/constants.py
@@ -1,17 +1,17 @@
 TRANSFORMATION_1_FILE = "transformation_1/transformation_1.ktr"
 
-TRANSFORMATION_1_STEPS = {'Text file output': {'type': 'TextFileOutput'}, 'Filter rows': {'type': 'FilterRows'},
-                          'Dummy (do nothing)': {'type': 'Dummy'},
-                          'Text file input': {'type': 'TextFileInput'},
-                          'Select values': {'type': 'SelectValues'}}
+TRANSFORMATION_1_STEPS = {'Dummy (do nothing)': {'type': 'Dummy'}, 'Select values': {'type': 'SelectValues'},
+                          'Text file output': {'type': 'TextFileOutput'}, 'Filter rows': {'type': 'FilterRows'},
+                          'Text file input': {'type': 'TextFileInput'}, 'Dummy (do nothing) 2': {'type': 'Dummy'}}
 
-TRANSFORMATION_1_HOPS = [{'to': 'Select values', 'from': 'Text file input', 'enabled': True, 'error': False},
-                         {'to': 'Filter rows', 'from': 'Select values', 'enabled': True, 'error': False},
-                         {'to': 'Dummy (do nothing)', 'from': 'Filter rows', 'enabled': True, 'error': False},
-                         {'to': 'Text file output', 'from': 'Filter rows', 'enabled': True, 'error': False}]
+TRANSFORMATION_1_HOPS = [{'to': 'Select values', 'from': 'Text file input', 'enabled': 'Y'},
+                         {'to': 'Filter rows', 'from': 'Select values', 'enabled': 'Y'},
+                         {'to': 'Dummy (do nothing)', 'from': 'Filter rows', 'enabled': 'Y'},
+                         {'to': 'Text file output', 'from': 'Filter rows', 'enabled': 'Y'},
+                         {'to': 'Dummy (do nothing) 2', 'from': 'Select values', 'enabled': 'Y'}]
 
 TRANSFORMATION_1_GRAPH = {'Text file input': ['Select values'],
                           'Filter rows': ['Dummy (do nothing)', 'Text file output'],
-                          'Select values': ['Filter rows']}
+                          'Select values': ['Filter rows', 'Dummy (do nothing) 2']}
 
 TRANSFORMATION_1_PATHS = [['Text file input', 'Select values', 'Filter rows', 'Text file output']]
